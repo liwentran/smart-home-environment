77e91a0cac6be46f4e018ac90d957111f978329a
madHacks10000 <madeline.f.estey@gmail.com>
2022-01-28 18:08:55 -0500

    Added functionality for DHT11 temp/humid sensor, added to output.py, cleaned up output.py print statements

--- RaspberryPiCode/output.py
+++ RaspberryPiCode/output.py
@@ -1,4 +1,5 @@
 import sensors
+import dht11
 from time import sleep
 import RPi.GPIO as GPIO
 
@@ -8,17 +9,20 @@ def main():
     adc = sensors.MCP3008()
     touch_sensor = sensors.Touch(26)
     light = sensors.LED(18)
+    temp_and_humid = dht11.DHT11(pin = 14)
     light.blink_once()
     GPIO.setup(4, GPIO.IN)
 
     
     while True:
         print(touch_sensor)
-        print("photo sensor: ")
-        print(adc.read(channel = 0))
-        print("fire: ")
-        print(adc.read(channel = 1))
-        sleep(1)
+        print("photo sensor: ", adc.read(channel = 0))
+        print("fire: ", adc.read(channel = 1))
+        #print("temp and humid" , adc.read(channel = 2))
+        result = temp_and_humid.read()
+        print("Temp: ", result.temperature, "C", " Humidity: ", result.humidity, "%", sep = "")
+        print()
+        sleep(2)
 
 
 if __name__ == '__main__':
