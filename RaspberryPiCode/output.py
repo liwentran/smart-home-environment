2964adaeffec3928bb7e07234b28f149b57f2e99
Dakh121 <kdarin172@gmail.com>
2022-02-08 15:08:12 -0500

    Added comments and carbon monoxide readings to output file

--- RaspberryPiCode/output.py
+++ RaspberryPiCode/output.py
@@ -3,6 +3,7 @@ import dht11
 from time import sleep
 import RPi.GPIO as GPIO
 import smoke_detector
+#import carbon_monoxide
 
 # main loop
 def main():
@@ -19,14 +20,17 @@ def main():
         print(touch_sensor)
         print("Light Level: ", adc.read(channel = 0))
         print("Fire: ", adc.read(channel = 1))
-        print("Gas: ", adc.read(channel = 2))
+        print("Gas: ", adc.read(channel = 2)) #MQ2 Gas Smoke Sensor
         print("Water Level: ", adc.read(channel = 3))
         print("Vibration: ", adc.read(channel = 4))
         print("Sound Level: ", adc.read(channel = 5))
+        print("Carbon Monoxide: ", adc.read(channel = 7))
         #online datasheet indicates that dht11 has a 6 second response time
         if (count % 2 == 0):
             result = temp_and_humid.read()
             print("Temp: ", result.temperature, "C", " Humidity: ", result.humidity, "%", sep = "")
+        #if...
+        #display AD value and CO density percent separately
         print()
         count += 1
         sleep(1)
