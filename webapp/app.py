31e6fcdd5952c9ad81de1fd5e257784407b186f8
Dakh121 <kdarin172@gmail.com>
2022-04-19 14:35:50 -0400

    Flask web app work, started with JSON

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,36 @@
 from flask import Flask, render_template
 from flask import Flask
+from flask import request
+from flask import jsonify, make_response
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+      'time': timeString
+      }
+    return render_template('index.html', **templateData)
 
+@app.route('/postjson', methods=['POST'])
+def postJsonHandler():
+    if request.is_json:
+        req = request.get_json()
+        response_body = {
+            "message": "JSON received!",
+            "sender": req.get("name")
+        }
+        res = make_response(jsonify(response_body), 200)
+        return res
+        #print (req)
+        #return "JSON posted", 200
+    else:
+        #return "Request was not JSON", 400
+        return make_response(jsonify({"message": "Request body must be JSON"}), 400)
+    
 #@app.route('/secondPage')
 #def pageTwo():
     #return 'this is page 2'
